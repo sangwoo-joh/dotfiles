8dc130b2e7de6bd9393613960e83f8ad0d1261cd
Sangwoo Joh <swjoh@ropas.snu.ac.kr>
2017-07-12 16:59:41 +0900

    I'm not using elementary os anymore
     - Ubuntu gnome is the best OS in the world

--- elementary.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install software-properties-common
-sudo apt-get install nimf
-im-config -n nimf
-sudo apt-add-repository ppa:philip.scott/elementary-tweaks
-sudo apt-get update
-sudo apt-get install elementary-tweaks
