06e3f1342621f5e4462d6250b13cff9479fd32e5
Sangwoo Joh <swjoh@ropas.snu.ac.kr>
2017-08-02 16:59:34 +0900

    Load packages with use-package package

--- emacs/init.el
+++ emacs/init.el
@@ -1,16 +1,20 @@
+;; initial package setup
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
 (package-initialize)
- (require 'desktop)
-  (desktop-save-mode 1)
-  (defun my-desktop-save ()
-    (interactive)
-    ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
-    (if (eq (desktop-owner) (emacs-pid))
-        (desktop-save desktop-dirname)))
-  (add-hook 'auto-save-hook 'my-desktop-save)
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'desktop)
+(desktop-save-mode 1)
+(defun my-desktop-save ()
+  (interactive)
+  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+  (if (eq (desktop-owner) (emacs-pid))
+      (desktop-save desktop-dirname)))
+(add-hook 'auto-save-hook 'my-desktop-save)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -71,52 +75,12 @@
 (when (eq system-type 'darwin)
   (setq mac-command-modifier 'meta))
 
-(global-font-lock-mode 1)		; Syntax highlight
+(global-font-lock-mode 1)
 (show-paren-mode 1)
 (setq search-highlight t)
 (setq query-replace-highlight t)
-
 (setq TeX-PDF-mode t)
-
-(global-set-key (kbd "<C-tab>") 'select-next-window)
-(global-set-key (kbd "<C-S-tab>") 'select-previous-window)
-(global-set-key (kbd "<C-iso-lefttab>") 'select-previous-window)
-(global-set-key (kbd "C-x C-k") 'kill-this-buffer)
-(global-set-key (kbd "C-x C-n") 'next-buffer)
-(global-set-key (kbd "C-x C-p") 'previous-buffer)
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-(global-set-key (kbd "C-c ;") 'comment-region)
-(global-set-key (kbd "C-c :") 'uncomment-region)
-(global-set-key (kbd "C-`") 'up-down-case-char)
-(global-set-key (kbd "M-Q") 'unfill-paragraph)
-
-;; Bookmarks
-(global-set-key (kbd "C-x p") 'bookmark-jump)
-(global-set-key (kbd "C-x P") 'bookmark-set)
-
-(when window-system			; Disable suspend
-  (global-unset-key (kbd "C-z")))
-
-(if (eq system-type 'darwin)
-    (set-font-size 18)
-  (set-font-size 14))
-
-(global-linum-mode 1)
-
-;; Package settings
-
-;; multiple-cursors
-(global-set-key (kbd "C-x C-g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
-
-;; multiple-cursors
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; expand-region
-(global-set-key (kbd "C-=") 'er/expand-region)
+(setq tramp-default-method "sshx")
 
 ;; auto highlight
 (defun highlight-and-mark ()
@@ -133,67 +97,96 @@
   (interactive)
   (unhighlight-regexp t))
 
+;; keyboard shortcuts
 (global-set-key (kbd "C-c h") 'highlight-and-mark)
 (global-set-key (kbd "C-c H") 'unhighlight-all)
+(global-set-key (kbd "<C-tab>") 'select-next-window)
+(global-set-key (kbd "<C-S-tab>") 'select-previous-window)
+(global-set-key (kbd "<C-iso-lefttab>") 'select-previous-window)
+(global-set-key (kbd "C-x C-k") 'kill-this-buffer)
+(global-set-key (kbd "C-x C-n") 'next-buffer)
+(global-set-key (kbd "C-x C-p") 'previous-buffer)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-c ;") 'comment-region)
+(global-set-key (kbd "C-c :") 'uncomment-region)
+(global-set-key (kbd "C-`") 'up-down-case-char)
+(global-set-key (kbd "M-Q") 'unfill-paragraph)
+(global-set-key (kbd "C-x p") 'bookmark-jump)
+(global-set-key (kbd "C-x P") 'bookmark-set)
+
+(when window-system			; Disable suspend
+  (global-unset-key (kbd "C-z")))
+(if (eq system-type 'darwin)
+    (set-font-size 18)
+  (set-font-size 14))
+(global-linum-mode 1)
+
+;; Package settings
+;; use use-package
+;; to automatically setup & load package & setup package in the same block
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+	 ("C->" . mc/mark-next-like-this)
+	 ("C-<" . mc/mark-previous-like-this)
+	 ("C-c C-<" . mc/mark-all-like-this)))
+
+(use-package magit
+  :ensure t
+  :bind (("C-x C-g" . magit-status)
+	 ("C-x M-g" . magit-dispatch-popup)))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+(use-package rainbow-delimeters
+  :ensure t)
+
+(use-package popup
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package auto-complete
+  :ensure t
+  :bind ("C-c <tab>" . ac-complete-merlin))
+
+;; Color theme: dracula
+;; See https://github.com/zenorocha/dracula-theme
+(use-package dracula-theme
+  :ensure t)
+(load-theme 'dracula t)
+
+(use-package web-mode
+  :ensure t
+  :init
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-indent-style 2))
+
+(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 
 ;; OPAM packages: ocp-indent & merlin & tuareg
 ;; NOTE: they share a load path.
-(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+;; NOTE2: opam package cannot be set up by use-package...
+(setq opam-share
+      (substring
+       (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
 (require 'ocp-indent)
 (require 'merlin)
-;(require 'ocp-index)
 (add-hook 'tuareg-mode-hook 'merlin-mode t)
 (add-hook 'caml-mode-hook 'merlin-mode t)
-;install rainbow-delimiters
 (add-hook 'tuareg-mode-hook 'rainbow-delimiters-mode t)
 (add-hook 'caml-mode-hook 'rainbow-delimiters-mode t)
 (setq merlin-use-auto-complete-mode 'easy)
 (setq merlin-command 'opam)
 (load (concat opam-share "/emacs/site-lisp/tuareg-site-file"))
 
-;; auto-complete
-(add-to-list 'load-path "~/.emacs.d/elpa/popup-0.5")
-(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-1.4")
-(require 'auto-complete)
-(global-set-key (kbd "C-c <tab>") 'ac-complete-merlin)
-
-;; Color theme: dracula
-;; See https://github.com/zenorocha/dracula-theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;;(load-theme 'solarized t)
-(load-theme 'dracula t)
-
-;; NOTE: tuareg is installed from opam, and after that you should follow the tuaret output to .emacs file like (load "path-to-git-checkout-dir/tuareg-site-file")
 (display-time)
 (defun chomp-end (str)
-  "Chomp tailing whitespace from STR."
-  (replace-regexp-in-string (rx (* (any " \t\n")) eos)
-                            ""
-                            str))
-
-(let ((support-base-dir (concat (replace-regexp-in-string "refmt" "" (file-truename (chomp-end (shell-command-to-string "which refmt")))) ".."))
-      (merlin-base-dir (concat (replace-regexp-in-string "ocamlmerlin" "" (file-truename (chomp-end (shell-command-to-string "which ocamlmerlin")))) "..")))
-  ;; Add npm merlin.el to the emacs load path and tell emacs where to find ocamlmerlin
-  (add-to-list 'load-path (concat merlin-base-dir "/share/emacs/site-lisp/"))
-  (setq merlin-command (concat merlin-base-dir "/bin/ocamlmerlin"))
-
-  ;; Add npm reason-mode to the emacs load path and tell emacs where to find refmt
-  (add-to-list 'load-path (concat support-base-dir "/share/emacs/site-lisp"))
-  (setq refmt-command (concat support-base-dir "/bin/refmt")))
-
-(defun my-web-mode-hook ()
-  "Hooks for Web mode."
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    (setq web-mode-indent-style 2)
-)
-(add-hook 'web-mode-hook  'my-web-mode-hook)
-(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-
-; smt2 syntax checker for z3
-;(setq flycheck-z3-smt2-executable "/home/swjoh/git/AA/z3")
-
-(setq tramp-default-method "sshx")
+  (replace-regexp-in-string (rx (* (any " \t\n")) eos) "" str))
