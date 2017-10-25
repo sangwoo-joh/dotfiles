67538282b605de020db7e756a8b8c8e8bb70a8f9
Sangwoo Joh <swjoh@ropas.snu.ac.kr>
2017-10-25 17:07:05 +0900

    Change auto-complete from merlin to ocamlformat

--- emacs/init.el
+++ emacs/init.el
@@ -152,7 +152,7 @@
   :bind ("C-=" . er/expand-region))
 (use-package auto-complete
   :ensure t
-  :bind ("C-c <tab>" . ac-complete-merlin))
+  :bind ("C-c <tab>" . ocamlformat))
 ;; (use-package dracula-theme
 ;;   :ensure t)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
