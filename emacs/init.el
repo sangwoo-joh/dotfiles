833f3babc6b107ef24d3f4a41a32b1e8ff861fe4
Sangwoo Joh <swjoh@ropas.snu.ac.kr>
2018-06-14 22:01:00 +0900

    emacs package setting for c/c++ settings - aux and shortcuts

--- emacs/init.el
+++ emacs/init.el
@@ -241,3 +241,53 @@
 
 (use-package clang-format
   :ensure t)
+;; for C++
+(use-package company
+  :ensure t
+  :init
+  (setq company-idle-delay 0)
+  (progn
+  (add-hook 'c-mode-hook 'company-mode)
+  (add-hook 'c++-mode-hook 'company-mode)))
+
+(use-package irony
+  :ensure t
+  :init
+  (progn
+    (unless (irony--find-server-executable) (call-interactively #'irony-install-server))
+    (add-hook 'c-mode-hook 'irony-mode)
+    (add-hook 'c++-mode-hook 'irony-mode)
+    (setq-default irony-cdb-compilation-databases '(irony-cdb-libclang irony-cdb-clang-complete))
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
+
+(use-package flycheck
+  :ensure t)
+
+(use-package company-irony
+  :ensure t
+  :bind (("M-/" . company-irony)))
+
+(use-package flycheck-irony
+  :ensure t
+  :init
+  (progn
+    (eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))))
+
+(defun my-c-c++-mode-flycheck-hook ()
+  (interactive)
+  (flycheck-select-checker 'c/c++-clang)
+  (flycheck-mode))
+(progn
+  (add-hook 'c-mode-hook 'my-c-c++-mode-flycheck-hook)
+  (add-hook 'c++-mode-hook 'my-c-c++-mode-flycheck-hook))
+
+(use-package irony-eldoc
+  :ensure t
+  :init
+  (progn
+    (add-hook 'irony-mode-hook #'irony-eldoc)))
+
+(use-package clang-format
+  :ensure t
+  :bind (("C-c C-f" . clang-format-auto))
+  )
