75ea8fb7447627e0d0a8ddd2eb062240e7d45fea
Sangwoo Joh <sangwoo.joh@samsung.com>
2018-12-07 13:28:46 +0900

    Add python-mode

--- emacs/config/python.el
+++ emacs/config/python.el
@@ -1,6 +1,15 @@
-(add-hook 'python-mode-hook 'guess-style-guess-tabs-mode)
-(add-hook 'python-mode-hook (lambda ()
-			      (guess-style-guess-tab-width)))
+;; (add-hook 'python-mode-hook 'guess-style-guess-tabs-mode)
+;; (add-hook 'python-mode-hook (lambda ()
+;; 			      (guess-style-guess-tab-width)))
+(use-package python-mode
+  :ensure t
+  :init
+  (lambda ()
+    (setq-default tab-width 2)
+    (setq-default indent-tabs-mode t)
+    (setq-default py-indent-tabs-mode t)
+    (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+
 (use-package elpy
   :ensure t
   :init (elpy-enable))
