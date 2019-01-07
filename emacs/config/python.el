3f8a3ea0638ec9e707855cdc31df3aed5bebc883
Sangwoo Joh <sangwoo.joh@samsung.com>
2019-01-07 15:09:21 +0900

    Add anaconda mode

--- emacs/config/python.el
+++ emacs/config/python.el
@@ -10,6 +10,13 @@
     (setq-default py-indent-tabs-mode t)
     (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
+(use-package anaconda-mode
+  :ensure t
+  :bind (("C-c <tab>" . anaconda-mode-complete))
+  :init
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+
 (use-package elpy
   :ensure t
   :init (elpy-enable))
