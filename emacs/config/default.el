c23a3db3e069aa40efce28662bfde280449ea020
Sangwoo Joh <lambdasnow@gmail.com>
2019-08-27 16:57:30 +0900

    Change other-frame to C-c o
     C-c <tab> reserved at auto complete

--- emacs/config/default.el
+++ emacs/config/default.el
@@ -122,7 +122,7 @@
 (global-set-key (kbd "C-x P") 'bookmark-set)
 (global-set-key (kbd "C-c n") 'make-frame)
 (global-set-key (kbd "C-c k") 'delete-frame)
-(global-set-key (kbd "C-c <tab>") 'other-frame)
+(global-set-key (kbd "C-c o") 'other-frame)
 
 (when window-system			; Disable suspend
   (global-unset-key (kbd "C-z")))
