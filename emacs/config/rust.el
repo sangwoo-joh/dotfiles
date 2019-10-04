6c7fde78453fb1b82925d090b7713679814bb200
Sangwoo Joh <lambdasnow@gmail.com>
2019-10-04 13:09:22 +0900

    Add rust packages

--- emacs/config/rust.el
+++ emacs/config/rust.el
@@ -1,2 +1,12 @@
 ;; rust formatting: need to install `cargo install rustfmt` and add path `export PATH="$PATH:$HOME/.cargo/bin"
 (setq rust-format-on-save t)
+
+(use-package rust-mode :ensure t)
+
+;; Need to install racer: cargo install racer (nightly only)
+(use-package racer
+  :ensure t
+  :init
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode))
