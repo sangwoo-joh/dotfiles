d4d5ebb6244fc9d7a03bebaf33b2ced23f7bef62
Sangwoo Joh <lambdasnow@gmail.com>
2019-10-16 16:04:31 +0900

    Upgrade OCaml package settings

--- emacs/config/ocaml.el
+++ emacs/config/ocaml.el
@@ -1,15 +1,29 @@
-;; OPAM packages: ocp-indent & merlin & tuareg
+;; OPAM packages: ocp-indent & merlin & tuareg & ocamlformat
 ;; NOTE: they share a load path.
 ;; NOTE2: opam package cannot be set up by use-package...
 (setq opam-share
       (substring
        (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-(require 'ocp-indent)
-(require 'merlin)
-(setq merlin-use-auto-complete-mode 'easy)
-(setq merlin-command 'opam)
 
+;; OCaml-related emacs packages are installed from OPAM ...
+(require 'ocp-indent)
+
+;; ocamlformat setting
+(load (concat opam-share "/emacs/site-lisp/ocamlformat"))
+
+(require 'merlin)
+(setq merlin-command 'opam)
+;; merlin can use company-mode for TabNine autocompletion
+(add-hook 'merlin-mode-hook 'company-mode)
+
+;; merlin can use auto-complete
+(use-package auto-complete
+  :ensure t
+  :init (setq merlin-ac-setup 'easy)
+  :bind (("C-c <tab>" . auto-complete)))
+
+;; Tuareg mode is augmented mode for caml-mode
 (require 'tuareg)
 (add-hook 'tuareg-mode-hook 'merlin-mode t)
 (add-hook 'tuareg-mode-hook 'rainbow-delimiters-mode t)
@@ -24,10 +38,3 @@
 (add-hook 'caml-mode-hook 'merlin-mode t)
 (add-hook 'caml-mode-hook 'rainbow-delimiters-mode t)
 (load (concat opam-share "/emacs/site-lisp/tuareg-site-file"))
-;; ocamlformat setting
-(load (concat opam-share "/emacs/site-lisp/ocamlformat"))
-
-(use-package auto-complete
-  :ensure t
-  :bind
-  (("C-c <tab>" . ac-complete-merlin)))
