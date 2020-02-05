7c7e50d9357e5b0877f1b31c6c90fc9fc811de28
Sangwoo Joh <lambdasnow@gmail.com>
2020-02-05 14:06:02 +0900

    Update bookmark shortcuts

--- emacs/config/default.el
+++ emacs/config/default.el
@@ -119,11 +119,12 @@
 (global-set-key (kbd "C-c :") 'uncomment-region)
 (global-set-key (kbd "C-`") 'up-down-case-char)
 (global-set-key (kbd "M-Q") 'unfill-paragraph)
-(global-set-key (kbd "C-x p") 'bookmark-jump)
-(global-set-key (kbd "C-x P") 'bookmark-set)
 (global-set-key (kbd "C-c n") 'make-frame)
 (global-set-key (kbd "C-c k") 'delete-frame)
 (global-set-key (kbd "C-c o") 'other-frame)
+(global-set-key (kbd "C-c m") 'bookmark-jump)
+(global-set-key (kbd "C-c M") 'bookmark-set)
+(global-set-key (kbd "C-c L") 'list-bookmarks)
 
 (when window-system			; Disable suspend
   (global-unset-key (kbd "C-z")))
