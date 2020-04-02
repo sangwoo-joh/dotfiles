4f25b40b0b8dafe33ba84886fae6e20d41c52887
Sangwoo Joh <lambdasnow@gmail.com>
2020-04-02 16:08:46 +0900

    Reformat package.el

--- emacs/config/packages.el
+++ emacs/config/packages.el
@@ -1,6 +1,7 @@
 ;; Package settings
-;; use use-package
-;; to automatically setup & load package & setup package in the same block
+
+;; use use-package to automatically setup & load package & setup
+;; package in the same block
 (use-package company
   :ensure t
   :init
@@ -14,21 +15,17 @@
   :ensure t
   :init
   (progn
-    ;; (unless (irony--find-server-executable) (call-interactively #'irony-install-server))
     (add-hook 'c-mode-hook 'irony-mode)
-    (add-hook 'c++-mode-hook 'irony-mode)
-    ;; (setq-default irony-cdb-compilation-databases '(irony-cdb-libclang irony-cdb-clang-complete))
-    ;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-    ))
+    (add-hook 'c++-mode-hook 'irony-mode)))
 
-(use-package flycheck
-  :ensure t)
+(use-package flycheck :ensure t)
 
 (use-package hungry-delete
   :ensure t
   :init (global-hungry-delete-mode)
-  :bind (("S-<backspace>" . hungry-delete-backward)
-	 ("S-<delete>" . hungry-delete-forward)))
+  :bind
+  (("S-<backspace>" . hungry-delete-backward)
+    ("S-<delete>" . hungry-delete-forward)))
 
 (use-package rainbow-delimiters
   :ensure t
@@ -44,16 +41,17 @@
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
-	 ("\\.md\\'" . markdown-mode)
-	 ("\\.markdown\\'" . markdown-mode))
+          ("\\.md\\'" . markdown-mode)
+          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
 (use-package multiple-cursors
   :ensure t
-  :bind (("C-S-c C-S-c" . mc/edit-lines)
-	 ("C->" . mc/mark-next-like-this)
-	 ("C-<" . mc/mark-previous-like-this)
-	 ("C-c C-<" . mc/mark-all-like-this)))
+  :bind
+  (("C-S-c C-S-c" . mc/edit-lines)
+    ("C->" . mc/mark-next-like-this)
+    ("C-<" . mc/mark-previous-like-this)
+    ("C-c C-<" . mc/mark-all-like-this)))
 
 (use-package magit
   :ensure t
@@ -65,7 +63,8 @@
 
 (use-package auto-complete
   :ensure t
-  :init (global-auto-complete-mode t)
+  :init
+  (global-auto-complete-mode t)
   (ac-set-trigger-key "C-c <tab>"))
 
 (use-package web-mode
@@ -75,6 +74,7 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-indent-style 2))
+
 (add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 
@@ -104,17 +104,16 @@
   :init
   (setq switch-window-shortcut-style 'qwerty)
   (setq switch-window-qwerty-shortcuts
-	'("a" "s" "d" "f" "j" "k" "l" ";" "q" "w" "e" "r" "i" "o" "z" "x" "c" "v"))
-  :bind (("C-x o" . switch-window)
-	 ("C-x 1" . switch-window-then-maximize)
-	 ("C-x 2" . switch-window-then-split-below)
-	 ("C-x 3" . switch-window-then-split-right)
-	 ("C-x 0" . switch-window-then-delete)
-	 ("C-x 4 d" . switch-window-then-dired)
-	 ("C-x 4 f" . switch-window-then-find-file)
-	 ;; ("C-x 4 m" . switch-window-then-compose-mail)
-	 ;; ("C-x 4 r" . switch-window-then-find-file-read-only)
-	 ("C-x 4 0" . switch-window-then-kill-buffer)))
+    '("a" "s" "d" "f" "j" "k" "l" ";" "q" "w" "e" "r" "i" "o" "z" "x" "c" "v"))
+  :bind
+  (("C-x o" . switch-window)
+    ("C-x 1" . switch-window-then-maximize)
+    ("C-x 2" . switch-window-then-split-below)
+    ("C-x 3" . switch-window-then-split-right)
+    ("C-x 0" . switch-window-then-delete)
+    ("C-x 4 d" . switch-window-then-dired)
+    ("C-x 4 f" . switch-window-then-find-file)
+    ("C-x 4 0" . switch-window-then-kill-buffer)))
 
 (use-package yasnippet-snippets :ensure t)
 (use-package yasnippet-classic-snippets :ensure t)
@@ -128,8 +127,7 @@
   :ensure t
   :bind (("C-z" . undo-tree-visualize)))
 
-(use-package ag
-  :ensure t)
+(use-package ag :ensure t)
 
 (use-package neotree
   :ensure t
@@ -148,24 +146,25 @@
   (setq eyebrowse-mode-line-separator "; ")
   (custom-set-faces
     '(eyebrowse-mode-line-active
-       ((t (:foreground "black" :background "gold" :inherit (mode-line-emphasis))))
-       ))
-  :bind (("M-0" . eyebrowse-switch-to-window-config-0)
-          ("M-1" . eyebrowse-switch-to-window-config-1)
-          ("M-2" . eyebrowse-switch-to-window-config-2)
-          ("M-3" . eyebrowse-switch-to-window-config-3)
-          ("M-4" . eyebrowse-switch-to-window-config-4)
-          ("M-5" . eyebrowse-switch-to-window-config-5)
-          ("M-6" . eyebrowse-switch-to-window-config-6)
-          ("M-7" . eyebrowse-switch-to-window-config-7)
-          ("M-8" . eyebrowse-switch-to-window-config-8)
-          ("M-9" . eyebrowse-switch-to-window-config-9)))
+       ((t (:foreground "black" :background "gold" :inherit (mode-line-emphasis))))))
+  :bind
+  (("M-0" . eyebrowse-switch-to-window-config-0)
+    ("M-1" . eyebrowse-switch-to-window-config-1)
+    ("M-2" . eyebrowse-switch-to-window-config-2)
+    ("M-3" . eyebrowse-switch-to-window-config-3)
+    ("M-4" . eyebrowse-switch-to-window-config-4)
+    ("M-5" . eyebrowse-switch-to-window-config-5)
+    ("M-6" . eyebrowse-switch-to-window-config-6)
+    ("M-7" . eyebrowse-switch-to-window-config-7)
+    ("M-8" . eyebrowse-switch-to-window-config-8)
+    ("M-9" . eyebrowse-switch-to-window-config-9)))
 
-;; little alternative for grep?
+;; local grep
 (use-package swiper
   :ensure t
-  :bind (("C-c C-s" . swiper)
-          ("C-c C-a" . swiper-all)))
+  :bind
+  (("C-c C-s" . swiper)
+    ("C-c C-a" . swiper-all)))
 
 (use-package which-key
   :ensure t
@@ -191,11 +190,9 @@
 ;; C-c C-p: Toggle read-only area
 ;; C-c C-k: Discard all changes and exit
 ;; C-c C-q: Exit wgrep mode
-(use-package wgrep
-  :ensure t)
+(use-package wgrep :ensure t)
 
-(use-package wakatime-mode
-  :ensure t)
+(use-package wakatime-mode :ensure t)
 
 (use-package company-tabnine
   :ensure t
@@ -203,8 +200,7 @@
 
 ;; use as //docker:<container-id>
 ;; for remote: /sshx:<remote>|docker:<container-name>:<path> is fantastic. MAGIC CHARM.
-(use-package docker-tramp
-  :ensure t)
+(use-package docker-tramp :ensure t)
 
 (use-package helpful
   :ensure t
