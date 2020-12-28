1e1ab9089a9c648de0b481fb3619ba26a94146bd
Sangwoo Joh <lambdasnow@gmail.com>
2020-12-28 18:11:51 +0900

    Remove python mode

--- emacs/config/python.el
+++ emacs/config/python.el
@@ -1,12 +1,6 @@
 ;;; python.el --- Personal python settings.
 ;;; Commentary:
 ;;; Code:
-
-(use-package python-mode
-  :ensure t
-  :config
-  (setq-default tab-width 4))
-
 (use-package elpy
   :ensure t
   :config (elpy-enable))
