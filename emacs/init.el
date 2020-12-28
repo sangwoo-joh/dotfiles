2e9cd6325b55a09ff4f3fced79ba0d1868780de4
Sangwoo Joh <lambdasnow@gmail.com>
2020-12-28 17:15:28 +0900

    auto-package-update

--- emacs/init.el
+++ emacs/init.el
@@ -38,3 +38,10 @@
 
 ;; setting for others
 (load "~/.emacs.d/config/others.el")
+
+;; auto-package-update
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (auto-package-update-maybe))
