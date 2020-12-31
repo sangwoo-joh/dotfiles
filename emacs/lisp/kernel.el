125577bdf013204ada6c07c63efe7da9155e749e
Sangwoo Joh <lambdasnow@gmail.com>
2020-12-31 11:00:17 +0900

    Do not auto-save desktop

--- emacs/lisp/kernel.el
+++ emacs/lisp/kernel.el
@@ -14,10 +14,10 @@
   ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
   (if (eq (desktop-owner) (emacs-pid))
     (desktop-save desktop-dirname)))
-(setq desktop-auto-save-timeout 300)
 (add-hook 'auto-save-hook 'my-desktop-save)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+(setq desktop-auto-save-timeout nil)
 (setq auto-save-default nil)
 (setq auto-save-list-file-name nil)
 (setq make-backup-files nil)
