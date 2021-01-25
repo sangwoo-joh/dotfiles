c4ce01530859950ecf72e5b83173a12c8209c6fd
Sangwoo Joh <lambdasnow@gmail.com>
2021-01-25 14:03:41 +0900

    Update cpp settings: bind rtags-related only in c/c++ mode

--- emacs/lisp/cpp.el
+++ emacs/lisp/cpp.el
@@ -8,28 +8,37 @@
   :ensure t
   :bind (("C-c C-f" . clang-format-buffer)))
 
+(use-package rtags
+  :ensure t
+  :init (setq rtags-path "~/.dotfiles/rtags/bin"))
+
+(require 'rtags)
+
 (defun set-key-c++-mode ()
   (interactive)
   (define-key c++-mode-map (kbd "C-c ;") 'comment-line)
   (define-key c++-mode-map (kbd "C-c :") 'uncomment-region)
+  (define-key c++-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
+  (define-key c++-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
+  (define-key c++-mode-map (kbd "M-," ) 'rtags-location-stack-back)
+  (define-key c++-mode-map (kbd "C-,") 'rtags-location-stack-foward)
+  (define-key c++-mode-map (kbd "C-c C-t") 'rtags-symbol-type)
+  (define-key c++-mode-map (kbd "C-c C-r") 'rtags-rename-symbol)
+  (define-key c++-mode-map (kbd "C-c a")  'rtags-references-tree)
+
   (define-key c-mode-map (kbd "C-c ;") 'comment-line)
   (define-key c-mode-map (kbd "C-c :") 'uncomment-region)
+  (define-key c-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
+  (define-key c-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
+  (define-key c-mode-map (kbd "M-," ) 'rtags-location-stack-back)
+  (define-key c-mode-map (kbd "C-,") 'rtags-location-stack-foward)
+  (define-key c-mode-map (kbd "C-c C-t") 'rtags-symbol-type)
+  (define-key c-mode-map (kbd "C-c C-r") 'rtags-rename-symbol)
+  (define-key c-mode-map (kbd "C-c a")  'rtags-references-tree)
   (setq c-basic-offset 4))
 
 (add-hook 'c-mode-hook 'set-key-c++-mode)
 (add-hook 'c++-mode-hook 'set-key-c++-mode)
-
-(use-package rtags
-  :ensure t
-  :init (setq rtags-path "~/.dotfiles/rtags/bin")
-  :bind
-  ("M-." . rtags-find-symbol-at-point)
-  ("M-," . rtags-location-stack-back)
-  ("C-," . rtags-location-stack-foward)
-  ("C-c C-t" . rtags-symbol-type)
-  ("C-c C-r" . rtags-rename-symbol)
-  ("C-c a" . rtags-references-tree))
-
 
 (use-package company-rtags
   :ensure t
