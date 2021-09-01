ecf49a13ddf90da8a96b78629cf3fe5d43673aae
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-09-01 12:31:34 +0900

    :zap: apply performance tweaks
     I'm not sure this works for my settings...

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,22 @@
 ;;; init.el --- Personal emacs settings entry point
 ;;; Commentary:
 ;;; Code:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance Tweaks thanks to https://github.com/trev-dev/emacs
+;; Minimize gc during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setq gc-cons-threshold (expt 2 23))))
+
+(setq read-process-output-max (* 1024 1024))
+
+;; Don't die when handling large, minified files
+(global-so-long-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (setq package-enable-at-startup nil)
