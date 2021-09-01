df76ec9b6345339c1119387e4ca3fa7da06dda6b
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-09-01 16:43:18 +0900

    Use utf8 for language env

--- emacs/lisp/kernel.el
+++ emacs/lisp/kernel.el
@@ -82,6 +82,7 @@
 (set-face-attribute 'default nil :font "fontset-default")
 
 (set-language-environment-input-method "Korean")
+(set-language-environment "UTF-8")
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
