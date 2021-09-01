825766ed74ede06294119d9c737ddee894759637
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-09-01 16:56:50 +0900

    :recycle: refactor python mode and use :snake: as mode name

--- emacs/lisp/py.el
+++ emacs/lisp/py.el
@@ -1,13 +1,19 @@
 ;;; py.el --- Personal python settings.
 ;;; Commentary:
 ;;; Code:
+
+(defun python-hook ()
+  "PYTHON MODE HOOK"
+  (highlight-indentation-mode -1)
+  (setq mode-name "🐍"))
+
 (use-package elpy
   :ensure t
   :config
   (elpy-enable)
   (setq elpy-rpc-timeout nil)
   :init
-  (add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1))))
+  (add-hook 'elpy-mode-hook #'python-hook))
 
 (with-eval-after-load 'flycheck
   (setq flycheck-python-flake8-executable "python3"))
