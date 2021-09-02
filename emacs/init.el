211c6f96e1992a80e9a7674a4ad87c6f820e91e4
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-09-02 12:11:02 +0900

    :fire: bye bye dracula theme..

--- emacs/init.el
+++ emacs/init.el
@@ -36,12 +36,6 @@
 ;; Initialize - OS X compatible
 (exec-path-from-shell-initialize)
 
-;; custom dracula theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;; (byte-recompile-directory "~/.emacs.d/themes" 0)
-(native-compile-async "~/.emacs.d/themes" 'recursively)
-(load-theme 'dracula t)
-
 ;; ready to load
 (add-to-list 'load-path "~/.emacs.d/lisp")
 ;; (byte-recompile-directory "~/.emacs.d/lisp" 0)
