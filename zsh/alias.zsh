d505c0cabb0503695f62846fdecf6574e0c8e12c
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-09-03 22:51:26 +0900

    :fire: remove non-compatible aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,16 +1,9 @@
-alias update='sudo apt-get update -y && sudo apt-get upgrade -y  && rustup update'
 alias count='wc -l'
-alias cpuinfo='grep '"'"'model name'"'"' /proc/cpuinfo'
 alias dist="cat /etc/*-release | grep PRETTY | awk -F = '{print $2}'"
-alias apacherestart='sudo /etc/init.d/apache2 restart'
 alias vi='nvim'
-alias mem='cat /proc/meminfo | grep MemTotal'
-alias du='du -s -h'
-alias df='df --type=ext4 -h'
 alias grep='grep --color=auto'
 alias png='dot -Tpng -O'
 alias dotclear='rm -rf *.dot *.png'
-alias here='nautilus --browser .'
 alias diff='diff -u --color=auto'
 alias ssh-hosts="grep -P \"^Host ([^*]+)$\" $HOME/.ssh/config | sed 's/Host //'"
 alias jsonify='python -m json.tool'
