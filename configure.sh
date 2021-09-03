bc5d215fe1984eef7ad54fff24bf6a568b7a6b5d
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-09-03 18:40:28 +0900

    WIP: add configure script

---
+++ configure.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+WD=$(cd $(dirname "$0"); pwd -P)
+ln -s "$WD"/zsh "$HOME"/.zsh
+
+# package update
+DISTRO=$(uname -s)
+case $DISTRO in
+    Linux)
+        sudo apt-get update
+        sudo apt-get install m4 zip bubblewrap git --yes
+        ;;
+    Darwin)
+        /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        export PATH=$PATH:/opt/homebrew/bin
+        brew update
+        brew install gpatch git
+        ;;
+    *)
+        echo "$DISTRO: Not Supported"
+        exit 2
+        ;;
+esac
+
+# ocaml
+/bin/sh -c "sh <(curl -fsSL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)"
+opam init
+eval $(opam env)
+opam update
+opam switch create kernel ocaml-base-compiler.4.12.0
+
+# ready to go
