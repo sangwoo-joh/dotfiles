f9e44e90327f569bba1216417d17ea0300fc1e03
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-12-20 17:10:35 +0900

    return to dracula theme.

--- emacs/lisp/packages.el
+++ emacs/lisp/packages.el
@@ -171,7 +171,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-tomorrow-day t)
+  (load-theme 'doom-dracula t)
   (doom-themes-visual-bell-config)
   (doom-themes-neotree-config))
 
